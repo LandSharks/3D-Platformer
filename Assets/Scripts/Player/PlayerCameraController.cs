36de734ba8f71238ac5a423d6c1e5ca6bf6e1371
Stephen G <gimpelstephen1@gmail.com>
2020-08-30 19:59:01 -0700

    follow camera added

--- Assets/Scripts/Player/PlayerCameraController.cs
+++ Assets/Scripts/Player/PlayerCameraController.cs
@@ -18,30 +18,14 @@ namespace Player {
         }
 
         private void LateUpdate() {
-            Vector3 characterOffset = target.position + offset;
+            targetPosition = target.position + target.up * distanceUp - target.forward * distanceAway;
 
-            lookDirection = characterOffset - this.transform.position;
-            lookDirection.y = 0f;
-            lookDirection.Normalize();
-
-            Debug.DrawRay(this.transform.position, lookDirection, Color.cyan);
-            // Setting target position to be the correct offset
-            targetPosition = characterOffset + target.up * distanceUp - lookDirection * distanceAway;
-
-            //TODO: Remove debug code...? Maybe that sick end if would be better here
             Debug.DrawRay(target.position, Vector3.up * distanceUp, Color.red);
             Debug.DrawRay(target.position, -1f * target.forward * distanceAway, Color.blue);
             Debug.DrawLine(target.position, targetPosition, Color.magenta);
 
-            // making smooth transition between current position and new position
-            //transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * smooth);
-            SmoothPosition(this.transform.position, targetPosition);
-
-            // Ensuring camera faces target
+            transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * smooth);
             transform.LookAt(target);
-        }
-        private void SmoothPosition(Vector3 fromPos, Vector3 toPos) {
-            this.transform.position = Vector3.SmoothDamp(fromPos, toPos, ref velocityCamSmooth, 0.1f);
         }
     }
 }
